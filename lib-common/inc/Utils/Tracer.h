d3d566b8c82e99034c6720fdab9d0a5ac668ede4
Eugene Kurzberg <i.kurtsberg@samsung.com>
2015-10-20 09:36:14 +0300

    TizenRefApp-4904 Prepare infrastructure for build under GBS
    Change-Id: Iae509035cd32185643eb153b64043a376c21479b
    Signed-off-by: Eugene Kurzberg <i.kurtsberg@samsung.com>

---
+++ lib-common/inc/Utils/Tracer.h
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) 2015 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#ifndef UTILS_TRACER_H
+#define UTILS_TRACER_H
+
+#include <time.h>
+
+namespace Utils
+{
+	/**
+	 * @brief Code block logger. Writes logs when created and destroyed. Outputs elapsed time.
+	 */
+	class Tracer
+	{
+	public:
+		/**
+		 * @brief Create tracer and write entry log.
+		 * @param[in]   tag     Log tag
+		 * @param[in]   file    Current source file
+		 * @param[in]   func    Current function
+		 * @param[in]   line    Current source line
+		 */
+		Tracer(const char *tag, const char *file, const char *func, int line);
+		~Tracer();
+
+	private:
+		const char *m_Tag;
+		const char *m_File;
+		const char *m_Func;
+		timespec m_startTime;
+	};
+}
+
+#endif /* UTILS_TRACER_H */
---
+++ packaging/org.tizen.contacts.spec
@@ -0,0 +1,58 @@
+Name: org.tizen.contacts
+Version: 0.1
+Release: 1
+License: Apache-2.0
+Summary: Contacts and Phone Applications
+Group: Applications/Social
+Source: %{name}-%{version}.tar.gz
+
+BuildRequires: cmake
+BuildRequires: edje-bin
+BuildRequires: pkgconfig(capi-appfw-application)
+BuildRequires: pkgconfig(contacts-service2)
+BuildRequires: pkgconfig(dlog)
+BuildRequires: pkgconfig(efl-extension)
+BuildRequires: pkgconfig(elementary)
+
+%description
+Contacts and Phone Reference Applications.
+
+%define _pkgdir /usr/apps/%{name}
+%define _bindir %{_pkgdir}/bin
+%define _libdir %{_pkgdir}/lib
+%define _resdir %{_pkgdir}/res
+%define _manifestdir /usr/share/packages
+%define _icondir /usr/share/icons/default/small
+%define _smackdir /etc/smack/accesses.d
+
+%prep
+%setup -q
+
+%build
+cmake \
+	%{!?RELEASE: -DCMAKE_BUILD_TYPE=Debug }\
+	'-DPACKAGE=%{name}'\
+	'-DCMAKE_INSTALL_PREFIX=%{_pkgdir}'\
+	'-DBINDIR=%{_bindir}'\
+	'-DLIBDIR=%{_libdir}'\
+	'-DRESDIR=%{_resdir}'\
+	'-DMANIFESTDIR=%{_manifestdir}'\
+	'-DICONDIR=%{_icondir}'\
+	'-DSMACKDIR=%{_smackdir}'
+
+make %{?jobs:-j%jobs}
+
+%install
+%make_install
+
+%clean
+rm -rf %{buildroot}
+
+%files
+%defattr(-,root,root,-)
+%manifest %{name}.manifest
+%{_bindir}/*
+%{_libdir}/*.so
+%{_manifestdir}/%{name}.xml
+%{_icondir}/*.png
+%{_smackdir}/%{name}.efl
