74a15288b766ea10a10f252b5c22c6eb293def48
Eugene Kurzberg <i.kurtsberg@samsung.com>
2015-10-21 18:04:43 +0300

    TizenRefApp-4895 Implement main application architecture
    Change-Id: Iedb1cc35185236252597bc4b345f2319272ce158
    Signed-off-by: Eugene Kurzberg <i.kurtsberg@samsung.com>

---
+++ lib-common/src/Ui/Control.cpp
@@ -0,0 +1,64 @@
+/*
+ * Copyright (c) 2015 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#include "Ui/Control.h"
+#include "Utils/Callback.h"
+
+using namespace Ui;
+
+Control::Control()
+	: m_Object(nullptr)
+{
+}
+
+Control::~Control()
+{
+	if (m_Object) {
+		evas_object_del(reset());
+	}
+}
+
+Evas_Object *Control::create(Evas_Object *parent)
+{
+	if (!m_Object) {
+		m_Object = onCreate(parent);
+		evas_object_event_callback_add(m_Object, EVAS_CALLBACK_FREE,
+				makeCallback(&Control::onDestroy), this);
+	}
+
+	return m_Object;
+}
+
+Evas_Object *Control::getEvasObject() const
+{
+	return m_Object;
+}
+
+Evas_Object *Control::reset()
+{
+	Evas_Object *object = m_Object;
+	evas_object_event_callback_del(m_Object, EVAS_CALLBACK_FREE,
+			makeCallback(&Control::onDestroy));
+	m_Object = nullptr;
+	return object;
+}
+
+void Control::onDestroy(Evas *e, Evas_Object *obj, void *event_info)
+{
+	reset();
+	delete this;
+}
---
+++ lib-common/src/Ui/Navigator.cpp
@@ -0,0 +1,75 @@
+/*
+ * Copyright (c) 2015 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#include "Ui/Navigator.h"
+
+using namespace Ui;
+
+Navigator::Navigator(NavigatorType type)
+	: m_Type(type)
+{
+}
+
+void Navigator::navigateTo(View *view)
+{
+	if (!view || view == getCurrentView()) {
+		return;
+	}
+
+	notifyNavigation(getCurrentView(), false);
+
+	if (view->getNavigator(m_Type) != this) {
+		Navigator *stackNavi = m_StackNavi;
+		Navigator *tabNavi = m_TabNavi;
+		if (m_Type == StackNavigator) {
+			stackNavi = this;
+		} else {
+			tabNavi = this;
+		}
+
+		view->create(getEvasObject());
+		view->onViewAttached(m_Window, stackNavi, tabNavi);
+		view->onPageAttached(attachView(view));
+	}
+
+	navigateToView(view);
+	notifyNavigation(view, true);
+}
+
+void Navigator::notifyNavigation(View *view, bool isCurrentView)
+{
+	if (view) {
+		view->onNavigation(isCurrentView);
+	}
+}
+
+void Navigator::onNavigation(bool isCurrentView)
+{
+	notifyNavigation(getCurrentView(), isCurrentView);
+}
+
+bool Navigator::onBackPressed()
+{
+	View *view = getCurrentView();
+	return view ? view->onBackPressed() : true;
+}
+
+Evas_Object *Navigator::onMenuPressed()
+{
+	View *view = getCurrentView();
+	return view ? view->onMenuPressed() : nullptr;
+}
