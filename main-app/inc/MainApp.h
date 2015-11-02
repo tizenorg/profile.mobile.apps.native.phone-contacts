2255d6310e7ca4f135d4258ad2b57e92bc39a1e5
Eugene Kurzberg <i.kurtsberg@samsung.com>
2015-11-02 14:10:50 +0200

    TizenRefApp-4925 Migrate application class
    Change-Id: Ib0220d5450756dc2960825fea65c7ab5131ac8ed
    Signed-off-by: Eugene Kurzberg <i.kurtsberg@samsung.com>

---
+++ main-app/inc/MainApp.h
@@ -0,0 +1,59 @@
+/*
+ * Copyright (c) 2015 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#ifndef MAIN_APP_H
+#define MAIN_APP_H
+
+#include "App/Application.h"
+
+namespace Ui
+{
+	class Navigator;
+	class Window;
+}
+
+class OperationController;
+
+class MainApp : public App::Application
+{
+public:
+	MainApp();
+
+	/**
+	 * @return Application main Window
+	 */
+	Ui::Window *getWindow() const;
+
+	/**
+	 * @return Application main Navigator
+	 */
+	Ui::Navigator *getNavigator() const;
+
+private:
+	virtual bool onCreate() override;
+	virtual void onAppControl(app_control_h request) override;
+	virtual void onPause() override;
+	virtual void onResume() override;
+	virtual void onTerminate() override;
+	static void onLanguageChanged(app_event_info_h event, void *data);
+
+	Ui::Window *m_Window;
+	Ui::Navigator *m_Navigator;
+	OperationController *m_Controller;
+};
+
+#endif /* MAIN_APP_H */
