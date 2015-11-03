c730c66a305beec295d9946ce6999c4d0f0f52a9
Eugene Kurzberg <i.kurtsberg@samsung.com>
2015-11-03 09:35:18 +0200

    TizenRefApp-4925 Migrate application class
    Change-Id: Ib0220d5450756dc2960825fea65c7ab5131ac8ed
    Signed-off-by: Eugene Kurzberg <i.kurtsberg@samsung.com>

---
+++ main-app/src/OperationController.cpp
@@ -0,0 +1,93 @@
+/*
+ * Copyright (c) 2015 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#include "OperationController.h"
+
+#include <string.h>
+
+OperationController::OperationController(int supportedOperations)
+	: m_SupportedOperations(supportedOperations), m_Application(nullptr), m_Request(nullptr)
+{
+}
+
+OperationController::~OperationController()
+{
+	app_control_destroy(m_Request);
+}
+
+void OperationController::create(MainApp *application)
+{
+	m_Application = application;
+	onCreate();
+}
+
+void OperationController::request(Operation operation, app_control_h request)
+{
+	app_control_destroy(m_Request);
+	m_Request = request;
+	onRequest(operation, request);
+}
+
+bool OperationController::isOperationSupported(Operation operation) const
+{
+	return (m_SupportedOperations & operation) != 0;
+}
+
+Operation OperationController::getOperation(const char *operation)
+{
+	if (!operation) {
+		return OPERATION_DEFAULT;
+	}
+	if (strcmp(operation, APP_CONTROL_OPERATION_DEFAULT) == 0) {
+		return OPERATION_DEFAULT;
+	}
+	if (strcmp(operation, APP_CONTROL_OPERATION_DIAL) == 0) {
+		return OPERATION_DIAL;
+	}
+	if (strcmp(operation, APP_CONTROL_OPERATION_ADD) == 0) {
+		return OPERATION_ADD;
+	}
+	if (strcmp(operation, APP_CONTROL_OPERATION_EDIT) == 0) {
+		return OPERATION_EDIT;
+	}
+	if (strcmp(operation, APP_CONTROL_OPERATION_VIEW) == 0) {
+		return OPERATION_VIEW;
+	}
+	if (strcmp(operation, APP_CONTROL_OPERATION_PICK) == 0) {
+		return OPERATION_PICK;
+	}
+
+	return OPERATION_DEFAULT;
+}
+
+MainApp *OperationController::getApplication() const
+{
+	return m_Application;
+}
+
+app_control_h OperationController::getRequest() const
+{
+	return m_Request;
+}
+
+void OperationController::replyFailure()
+{
+	app_control_h reply;
+	app_control_create(&reply);
+	app_control_reply_to_launch_request(reply, m_Request, APP_CONTROL_RESULT_FAILED);
+	app_control_destroy(reply);
+}
---
+++ main-app/src/OperationDefaultController.cpp
@@ -0,0 +1,127 @@
+/*
+ * Copyright (c) 2015 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#include "OperationDefaultController.h"
+
+#include "MainApp.h"
+#include "Ui/TabView.h"
+
+#include <app_preference.h>
+#include <badge.h>
+
+#define APP_CONTROL_PHONE_APPID "org.tizen.phone"
+#define APP_CONTROL_URI_DIAL "tel:"
+
+#define PREFERENCE_KEY_LAST_TAB "last_tab"
+
+OperationDefaultController::OperationDefaultController()
+	: OperationController(OPERATION_DEFAULT | OPERATION_DIAL),
+	  m_Navigator(nullptr), m_Tabs{nullptr}
+{
+}
+
+OperationDefaultController::~OperationDefaultController()
+{
+	saveLastTab();
+}
+
+void OperationDefaultController::onCreate()
+{
+	Ui::Navigator *mainNavigator = getApplication()->getNavigator();
+	m_Navigator = new Ui::TabView();
+	mainNavigator->navigateTo(m_Navigator);
+
+	/* TODO:
+	m_Tabs[TabDialer] = new Phone::DialerView();
+	m_Tabs[TabLogs] = new Phone::Logs::ListView();
+	m_Tabs[TabContacts] = new Contacts::ListView();
+	 */
+
+	for (auto &&tab : m_Tabs) {
+		m_Navigator->navigateTo(tab);
+	}
+}
+
+void OperationDefaultController::onRequest(Operation operation, app_control_h request)
+{
+	char *appId = nullptr;
+	app_control_get_app_id(request, &appId);
+
+	TabId selectedTab = TabContacts;
+	if (operation == OPERATION_DIAL) {
+		/* TODO:
+		Phone::DialerView *dialer = static_cast<Phone::DialerView *>(m_Tabs[TabDialer]);
+		dialer->setNumber(getPhoneNumber(request));
+		 */
+
+		selectedTab = TabDialer;
+	} else if (appId && strcmp(appId, APP_CONTROL_PHONE_APPID) == 0) {
+		if (getBadgeCount(APP_CONTROL_PHONE_APPID) > 0) {
+			selectedTab = TabLogs;
+		} else {
+			selectedTab = getLastTab();
+		}
+	}
+
+	m_Navigator->navigateTo(m_Tabs[selectedTab]);
+
+	free(appId);
+}
+
+std::string OperationDefaultController::getPhoneNumber(app_control_h appControl)
+{
+	std::string number;
+
+	char *uri = nullptr;
+	app_control_get_uri(appControl, &uri);
+
+	size_t length = sizeof(APP_CONTROL_URI_DIAL) - 1;
+	if (uri && strncmp(uri, APP_CONTROL_URI_DIAL, length) == 0) {
+		number = uri + length;
+	}
+
+	free(uri);
+	return number;
+}
+
+unsigned OperationDefaultController::getBadgeCount(const char *package)
+{
+	unsigned count = 0;
+	badge_get_count(package, &count);
+	return count;
+}
+
+void OperationDefaultController::saveLastTab()
+{
+	Ui::View *currentTab = m_Navigator->getCurrentView();
+	for (int tab = TabFirst; tab < TabMax; ++tab) {
+		if (currentTab == m_Tabs[tab]) {
+			if (tab != TabContacts) {
+				preference_set_int(PREFERENCE_KEY_LAST_TAB, tab);
+			}
+
+			break;
+		}
+	}
+}
+
+OperationDefaultController::TabId OperationDefaultController::getLastTab()
+{
+	TabId lastTab = TabDialer;
+	preference_get_int(PREFERENCE_KEY_LAST_TAB, (int *) &lastTab);
+	return lastTab;
+}
