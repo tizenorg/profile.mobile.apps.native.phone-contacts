5c9f1b7cda5ed1bfbbb09f54f3d65c492c2d64ab
Eugene Kurzberg <i.kurtsberg@samsung.com>
2015-11-04 15:43:58 +0200

    TizenRefApp-4925 Migrate application class
    Change-Id: Ib0220d5450756dc2960825fea65c7ab5131ac8ed
    Signed-off-by: Eugene Kurzberg <i.kurtsberg@samsung.com>

---
+++ main-app/inc/OperationController.h
@@ -0,0 +1,115 @@
+/*
+ * Copyright (c) 2015 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#ifndef OPERATION_CONTROLLER_H
+#define OPERATION_CONTROLLER_H
+
+#include <app_control.h>
+
+enum Operation
+{
+	OPERATION_DEFAULT = 1 << 0,
+	OPERATION_DIAL    = 1 << 1,
+	OPERATION_ADD     = 1 << 2,
+	OPERATION_EDIT    = 1 << 3,
+	OPERATION_VIEW    = 1 << 4,
+	OPERATION_PICK    = 1 << 5
+};
+
+class MainApp;
+
+/**
+ * @brief Handles operations requested through App Control
+ */
+class OperationController
+{
+public:
+	virtual ~OperationController();
+
+	/**
+	 * @brief Create operation controller
+	 * @param[in]   application     Main application instance
+	 */
+	void create(MainApp *application);
+
+	/**
+	 * @brief Request the controller to handle the operation
+	 */
+	void request(Operation operation, app_control_h request);
+
+	/**
+	 * @brief Called when application receives pause event
+	 */
+	virtual void onPause() { }
+
+	/**
+	 * @brief Called when application receives resume event
+	 */
+	virtual void onResume() { }
+
+	/**
+	 * @brief Check if operation is supported by the controller
+	 */
+	bool isOperationSupported(Operation operation) const;
+
+	/**
+	 * @brief Get operation from string representation
+	 * @param[in]   operation   Operation string representation
+	 * @return Operation value
+	 */
+	static Operation getOperation(const char *operation);
+
+protected:
+	/**
+	 * @brief Create operation controller
+	 * @param[in]   supportedOperations     Supported operations mask
+	 * @see Operation
+	 */
+	OperationController(int supportedOperations);
+
+	/**
+	 * @return Main application instance
+	 */
+	MainApp *getApplication() const;
+
+	/**
+	 * @return Last received request
+	 */
+	app_control_h getRequest() const;
+
+	/**
+	 * @brief Send failure reply to the last request
+	 */
+	void replyFailure();
+
+	/**
+	 * @brief Called after create() is called
+	 */
+	virtual void onCreate() { }
+
+	/**
+	 * @brief Called when application receives App Control request
+	 */
+	virtual void onRequest(Operation operation, app_control_h request) = 0;
+
+private:
+	int m_SupportedOperations;
+	MainApp *m_Application;
+	app_control_h m_Request;
+};
+
+#endif /* OPERATION_CONTROLLER_H */
---
+++ main-app/src/MainApp.cpp
@@ -0,0 +1,138 @@
+/*
+ * Copyright (c) 2015 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#include <contacts.h>
+
+#include "MainApp.h"
+#include "Ui/Window.h"
+#include "Ui/Naviframe.h"
+#include "Utils/Logger.h"
+
+#include "OperationDefaultController.h"
+/* TODO:
+#include "OperationEditController.h"
+#include "OperationViewController.h"
+#include "OperationPickController.h"
+ */
+
+#define APP_BASE_SCALE 2.6
+
+MainApp::MainApp()
+	: m_Window(nullptr), m_Navigator(nullptr), m_Controller(nullptr)
+{
+}
+
+Ui::Window *MainApp::getWindow() const
+{
+	return m_Window;
+}
+
+Ui::Navigator *MainApp::getNavigator() const
+{
+	return m_Navigator;
+}
+
+bool MainApp::onCreate()
+{
+	int err = contacts_connect();
+	RETVM_IF(err != CONTACTS_ERROR_NONE, false, "contacts_connect() failed(%d)", err);
+
+	app_event_handler_h handle = nullptr;
+	ui_app_add_event_handler(&handle, APP_EVENT_LANGUAGE_CHANGED,
+			&MainApp::onLanguageChanged, this);
+
+	elm_app_base_scale_set(APP_BASE_SCALE);
+
+	m_Window = new Ui::Window();
+	m_Window->create(nullptr);
+
+	m_Navigator = new Ui::Naviframe();
+	m_Window->attachView(m_Navigator);
+
+	return true;
+}
+
+void MainApp::onAppControl(app_control_h request)
+{
+	char *operationStr = nullptr;
+	app_control_get_operation(request, &operationStr);
+
+	Operation operation = OperationController::getOperation(operationStr);
+	if (!m_Controller || !m_Controller->isOperationSupported(operation)) {
+		delete m_Controller;
+		m_Controller = nullptr;
+
+		switch (operation) {
+			case OPERATION_DEFAULT:
+			case OPERATION_DIAL:
+				m_Controller = new OperationDefaultController();
+				break;
+			case OPERATION_ADD:
+			case OPERATION_EDIT:
+				/* TODO: m_Controller = new OperationEditController(); */
+				break;
+			case OPERATION_VIEW:
+				/* TODO: m_Controller = new OperationViewController(); */
+				break;
+			case OPERATION_PICK:
+				/* TODO: m_Controller = new OperationPickController(); */
+				break;
+		}
+
+		if (m_Controller) {
+			m_Controller->create(this);
+		}
+	}
+
+	if (m_Controller) {
+		m_Controller->request(operation, request);
+	}
+
+	free(operationStr);
+}
+
+void MainApp::onPause()
+{
+	if (m_Controller) {
+		m_Controller->onPause();
+	}
+}
+
+void MainApp::onResume()
+{
+	if (m_Controller) {
+		m_Controller->onResume();
+	}
+}
+
+void MainApp::onTerminate()
+{
+	delete m_Controller;
+	m_Controller = nullptr;
+
+	contacts_disconnect();
+}
+
+void MainApp::onLanguageChanged(app_event_info_h event, void *data)
+{
+	char *lang = nullptr;
+	app_event_get_language(event, &lang);
+	if (lang) {
+		elm_language_set(lang);
+		free(lang);
+	}
+}
