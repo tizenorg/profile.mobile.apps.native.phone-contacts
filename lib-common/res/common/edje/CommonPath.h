6d98f1f68db507bd4d057f64b115708cc1a24460
Eugene Kurzberg <i.kurtsberg@samsung.com>
2015-11-05 17:58:13 +0200

    TizenRefApp-4978 Implement Thumbnail utility
    Change-Id: I555802b91932e330e8dd64dbdd09beb0e3a2006e
    Signed-off-by: Eugene Kurzberg <i.kurtsberg@samsung.com>

---
+++ lib-common/res/common/edje/CommonPath.h
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2015 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#ifndef COMMON_PATH_H
+#define COMMON_PATH_H
+
+#define COMMON_IMG_DIR "common/images/"
+#define COMMON_EDJ_DIR "common/edje/"
+
+#define COMMON_THUMBNAIL_EDJ        COMMON_EDJ_DIR"common-thumbnail.edj"
+
+#define THUMBNAIL_DEFAULT_MEDIUM    COMMON_IMG_DIR"contacts_caller_id_default.png"
+#define THUMBNAIL_DEFAULT_SMALL     COMMON_IMG_DIR"contacts_caller_id_default_02.png"
+#define THUMBNAIL_DEFAULT_LARGE     COMMON_IMG_DIR"contacts_caller_id_default_03.png"
+
+#define THUMBNAIL_BG_MEDIUM         COMMON_IMG_DIR"contacts_caller_id_default_bg.png"
+#define THUMBNAIL_BG_SMALL          COMMON_IMG_DIR"contacts_caller_id_default_bg_02.png"
+#define THUMBNAIL_BG_LARGE          COMMON_IMG_DIR"contacts_caller_id_default_bg_03.png"
+
+#endif /* COMMON_PATH_H */
