6d74dc281e8abb89111f9618f8d7f92ed6d9d07a
Iryna Ferenchak <i.ferenchak@samsung.com>
2015-12-07 13:11:04 +0200

    TizenRefApp-5180 Implement LogRecord
    Change-Id: Ia225d893cd049549efeb00168b1f8c178d8e32da
    Signed-off-by: Iryna Ferenchak <i.ferenchak@samsung.com>

---
+++ lib-logs/src/Logs/Model/LogRecord.cpp
@@ -0,0 +1,78 @@
+/*
+ * Copyright (c) 2015 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#include "Logs/Model/LogRecord.h"
+#include "Utils/Logger.h"
+
+using namespace Logs::Model;
+
+LogRecord::LogRecord(contacts_record_h record)
+	: m_Record(record)
+{
+}
+
+LogRecord::~LogRecord()
+{
+	contacts_record_destroy(m_Record, true);
+}
+
+bool LogRecord::isGroup() const
+{
+	return false;
+}
+
+const char *LogRecord::getName() const
+{
+	char *name = nullptr;
+	contacts_record_get_str_p(m_Record, _contacts_person_phone_log.display_name, &name);
+	return name;
+}
+
+const char *LogRecord::getNumber() const
+{
+	char *number = nullptr;
+	contacts_record_get_str_p(m_Record, _contacts_person_phone_log.address, &number);
+	return number;
+}
+
+const char *LogRecord::getImagePath() const
+{
+	char *path = nullptr;
+	contacts_record_get_str_p(m_Record, _contacts_person_phone_log.image_thumbnail_path, &path);
+	return path;
+}
+
+int LogRecord::getTime() const
+{
+	int time = 0;
+	contacts_record_get_int(m_Record, _contacts_person_phone_log.log_time, &time);
+	return time;
+}
+
+int LogRecord::getType() const
+{
+	int type = CONTACTS_PLOG_TYPE_NONE;
+	contacts_record_get_int(m_Record, _contacts_person_phone_log.log_type, &type);
+	return type;
+}
+
+int LogRecord::getId() const
+{
+	int id = 0;
+	contacts_record_get_int(m_Record, _contacts_person_phone_log.person_id, &id);
+	return id;
+}
