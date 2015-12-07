5af2d49cb95731cf8b75bcc690b46327809cc8c7
Eugene Kurzberg <i.kurtsberg@samsung.com>
2015-12-07 12:53:06 +0200

    TizenRefApp-5201 Implement genlist item for typed object
    Change-Id: I7d64d0015ec34b0f17a87cca78e2f09eb26da52c
    Signed-off-by: Eugene Kurzberg <i.kurtsberg@samsung.com>

---
+++ lib-contact/res/input/edje/InputItemLayoutMetrics.h
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2015 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#ifndef INPUT_ITEM_LAYOUT_METRICS_H
+#define INPUT_ITEM_LAYOUT_METRICS_H
+
+#define ITEM_W 720
+#define ITEM_H 120
+
+#define ITEM_PADDING_Y 20
+
+#define ITEM_LEFT_W 154
+#define ITEM_LEFT_X1 32
+#define ITEM_LEFT_X2 32
+
+#define ITEM_RIGHT_W 75
+#define ITEM_RIGHT_X1 2
+#define ITEM_RIGHT_X2 10
+
+#endif /* INPUT_ITEM_LAYOUT_METRICS_H */
---
+++ lib-contact/src/Contacts/Input/ContactObjectItem.cpp
@@ -0,0 +1,61 @@
+/*
+ * Copyright (c) 2015 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#include "Contacts/Input/ContactObjectItem.h"
+#include "Utils/Callback.h"
+#include "InputItemLayout.h"
+
+using namespace Contacts::Input;
+using namespace Contacts::Model;
+
+ContactObjectItem::ContactObjectItem(ContactFieldPtr object)
+	: ContactFieldItem(*static_cast<ContactObject *>(object.get())->begin(), ELM_GENLIST_ITEM_GROUP),
+	  m_Object(std::move(object))
+{
+}
+
+void ContactObjectItem::setRemoveCallback(RemoveCallback callback)
+{
+	m_OnRemove = std::move(callback);
+}
+
+ContactObject *ContactObjectItem::getObject() const
+{
+	return static_cast<ContactObject *>(m_Object.get());
+}
+
+Evas_Object *ContactObjectItem::getContent(Evas_Object *parent, const char *part)
+{
+	if (strcmp(part, PART_RIGHT) == 0) {
+		Evas_Object *button = elm_button_add(parent);
+		elm_object_style_set(button, "icon_expand_delete");
+		evas_object_smart_callback_add(button, "clicked",
+				makeCallback(&ContactObjectItem::onRemovePressed), this);
+
+		return button;
+	} else {
+		return ContactFieldItem::getContent(parent, part);
+	}
+}
+
+void ContactObjectItem::onRemovePressed(Evas_Object *button, void *eventInfo)
+{
+	if (m_OnRemove) {
+		m_OnRemove(std::move(m_Object));
+	}
+	elm_object_item_del(getObjectItem());
+}
