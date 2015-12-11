3c5fb30215439784fdb56a6a6e682ee4d41e8e35
Iryna Ferenchak <i.ferenchak@samsung.com>
2015-12-11 13:18:17 +0200

    TizenRefApp-5218 Implement Log Group
    Change-Id: I1093995b155cdec6b13fe3990803e84c5f2e4f51
    Signed-off-by: Iryna Ferenchak <i.ferenchak@samsung.com>

---
+++ lib-logs/inc/Logs/Model/LogType.h
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2015 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#ifndef LOGS_MODEL_LOG_TYPE_H
+#define LOGS_MODEL_LOG_TYPE_H
+
+#include <list>
+#include <memory>
+
+namespace Logs
+{
+	namespace Model
+	{
+		class Log;
+		class LogGroup;
+
+		/**
+		 * @brief Smart pointer to Log.
+		 */
+		typedef std::shared_ptr<Log> LogPtr;
+
+		/**
+		 * @brief List of logs.
+		 */
+		typedef std::list<LogPtr> LogList;
+
+		/**
+		 * @brief Smart pointer of LogGroup.
+		 */
+		typedef std::shared_ptr<LogGroup> LogGroupPtr;
+	}
+}
+
+#endif /* LOGS_MODEL_LOG_TYPE_H */
