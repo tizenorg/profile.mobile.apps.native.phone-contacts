8aa659ec38d624857e22aba6db27cfaeb2db88e2
Iryna Ferenchak <i.ferenchak@samsung.com>
2015-12-16 09:53:39 +0200

    TizenRefApp-5219 Implement Log Provider
    Change-Id: Ib5297c5e0c24efdf699aa6f92691f1d6f87e1da0
    Signed-off-by: Iryna Ferenchak <i.ferenchak@samsung.com>

---
+++ lib-logs/inc/Logs/Model/LogProvider.h
@@ -0,0 +1,58 @@
+/*
+ * Copyright (c) 2015 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#ifndef LOGS_MODEL_LOG_PROVIDER_H
+#define LOGS_MODEL_LOG_PROVIDER_H
+
+#include "Logs/Model/LogType.h"
+
+namespace Logs
+{
+	namespace Model
+	{
+		/**
+		 * @brief Provides list of logs
+		 */
+		class LogProvider
+		{
+		public:
+			/**
+			 * @brief Constructor
+			 */
+			LogProvider();
+
+			/**
+			 * @brief Get logs list
+			 * @return list of logs.
+			 */
+			const LogList &getLogsList();
+
+		private:
+			void fillList();
+			bool isLogToGroup(LogPtr log, LogPtr lastLog);
+			LogGroupPtr groupLogs(LogPtr log, LogPtr lastLog);
+
+			void onLogChanged(const char *viewUri);
+			void onContactChanged(const char *viewUri);
+
+			LogList m_AllLogs;
+			int m_DbVersion;
+		};
+	}
+}
+
+#endif /* LOGS_MODEL_LOG_PROVIDER_H */
