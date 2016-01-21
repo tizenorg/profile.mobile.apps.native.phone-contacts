677ca9ae91a6557ce463682dc7a63bbd7a19b3fb
Eugene Kurzberg <i.kurtsberg@samsung.com>
2016-01-21 15:26:13 +0200

    Eliminated code duplication between ContactTextFieldControl and ContactCompoundObjectControl. Moved "Next/Done" feature to ContactTextFieldControl.
    Change-Id: I325349d5fe6c2cd6423cae83bac8ea46e29437e9
    Signed-off-by: Eugene Kurzberg <i.kurtsberg@samsung.com>

--- lib-contact/src/Contacts/Input/ContactFieldSubItem.cpp
+++ lib-contact/src/Contacts/Input/ContactFieldSubItem.cpp
@@ -28,7 +28,7 @@ using namespace Contacts::Input;
 using namespace Contacts::Model;
 
 ContactFieldSubItem::ContactFieldSubItem(ContactField &field)
-	: m_Field(field), m_NextFocusItem(nullptr)
+	: m_Field(field)
 {
 }
 
@@ -56,11 +56,21 @@ Elm_Genlist_Item_Class *ContactFieldSubItem::getItemClass() const
 
 Evas_Object *ContactFieldSubItem::getContent(Evas_Object *parent, const char *part)
 {
+	Ui::Control *control = nullptr;
 	if (strcmp(part, PART_MIDDLE) == 0) {
-		return createFieldControl(parent)->getEvasObject();
+		switch (m_Field.getType()) {
+			case TypeText:
+				control= new ContactTextFieldControl(this, m_Field.cast<ContactTextField>());
+				break;
+			case TypeDate:
+				control = new ContactDateFieldControl(m_Field.cast<ContactDateField>());
+				break;
+			default:
+				break;
+		}
 	}
 
-	return nullptr;
+	return control ? control->create(parent) : nullptr;
 }
 
 Eina_Bool ContactFieldSubItem::getState(Evas_Object *parent, const char *part)
@@ -79,59 +89,5 @@ void ContactFieldSubItem::onFocused()
 		} else if (m_Field.getType() == TypeDate) {
 			static_cast<ContactDateFieldControl *>(control)->showPicker();
 		}
-	}
-}
-
-Ui::Control *ContactFieldSubItem::createFieldControl(Evas_Object *parent)
-{
-	switch (m_Field.getType()) {
-		case TypeText:
-		{
-			auto control = new ContactTextFieldControl(m_Field.cast<ContactTextField>());
-			control->create(parent);
-
-			enableEntryReturnButton(control->getEntry());
-			return control;
-		}
-		case TypeDate:
-		{
-			auto control = new ContactDateFieldControl(m_Field.cast<ContactDateField>());
-			control->create(parent);
-			return control;
-		}
-		default:
-			return nullptr;
-	}
-}
-
-void ContactFieldSubItem::enableEntryReturnButton(Evas_Object *entry)
-{
-	evas_object_smart_callback_add(entry, "focused",
-			makeCallback(&ContactFieldSubItem::onEntryFocused), this);
-	evas_object_smart_callback_add(entry, "activated",
-			makeCallback(&ContactFieldSubItem::onEntryActivated), this);
-}
-
-void ContactFieldSubItem::onEntryFocused(Evas_Object *entry, void *eventInfo)
-{
-	m_NextFocusItem = nullptr;
-	for (auto item = getNextItem(); item; item = item->getNextItem()) {
-		if (item->isFocusable()) {
-			m_NextFocusItem = item;
-			break;
-		}
-	}
-
-	elm_entry_input_panel_return_key_type_set(entry, m_NextFocusItem
-			? ELM_INPUT_PANEL_RETURN_KEY_TYPE_NEXT
-			: ELM_INPUT_PANEL_RETURN_KEY_TYPE_DONE);
-}
-
-void ContactFieldSubItem::onEntryActivated(Evas_Object *entry, void *eventInfo)
-{
-	if (m_NextFocusItem) {
-		m_NextFocusItem->focus(ELM_GENLIST_ITEM_SCROLLTO_IN, true);
-	} else {
-		elm_object_focus_set(entry, EINA_FALSE);
 	}
 }
