914958b399bf7a59fb287d2bd650f4e5015ba664
Eugene Kurzberg <i.kurtsberg@samsung.com>
2016-01-21 15:26:13 +0200

    Eliminated code duplication between ContactTextFieldControl and ContactCompoundObjectControl. Moved "Next/Done" feature to ContactTextFieldControl.
    Change-Id: I325349d5fe6c2cd6423cae83bac8ea46e29437e9
    Signed-off-by: Eugene Kurzberg <i.kurtsberg@samsung.com>

--- lib-contact/src/Contacts/Input/ContactTextFieldControl.cpp
+++ lib-contact/src/Contacts/Input/ContactTextFieldControl.cpp
@@ -18,6 +18,8 @@
 #include "Contacts/Input/ContactTextFieldControl.h"
 #include "Contacts/Model/ContactTextField.h"
 #include "Contacts/Common/Strings.h"
+
+#include "Ui/GenlistItem.h"
 #include "Utils/Callback.h"
 #include "Utils/Range.h"
 
@@ -33,48 +35,109 @@ namespace
 	};
 }
 
-ContactTextFieldControl::ContactTextFieldControl(Model::ContactTextField &field)
-	: m_Field(field), m_IsEmpty(true)
+ContactTextFieldControl::ContactTextFieldControl(Ui::GenlistItem *parent,
+		Model::ContactTextField &field)
+	: m_ParentItem(parent), m_NextItem(nullptr), m_Field(field),
+	  m_IsEmpty(true), m_IsChanged(false), m_IsUpdating(false)
 {
 }
 
 void ContactTextFieldControl::save()
 {
+	if (!m_IsChanged) {
+		return;
+	}
+
 	char *text = elm_entry_markup_to_utf8(elm_entry_entry_get(getEntry()));
-	m_Field.setValue(text);
-	free(text);
+	if (text) {
+		setFieldValue(text);
+		free(text);
+	}
+
+	m_IsChanged = false;
 }
 
 void ContactTextFieldControl::update()
 {
-	char *text = elm_entry_utf8_to_markup(m_Field.getValue());
+	m_IsUpdating = true;
+
+	char *text = elm_entry_utf8_to_markup(getFieldValue().c_str());
 	elm_entry_entry_set(getEntry(), text);
 	free(text);
 }
 
-void ContactTextFieldControl::onCreated()
+void ContactTextFieldControl::updateEntryLayout()
 {
 	setGuideText(Common::getContactChildFieldName(m_Field.getId()));
+	elm_entry_input_panel_layout_set(getEntry(), Utils::at(inputLayout, m_Field.getSubType()));
+}
 
+std::string ContactTextFieldControl::getFieldValue() const
+{
+	const char *value = m_Field.getValue();
+	return value ? value : "";
+}
+
+void ContactTextFieldControl::setFieldValue(std::string value)
+{
+	m_Field.setValue(value.c_str());
+}
+
+void ContactTextFieldControl::onCreated()
+{
 	Evas_Object *entry = getEntry();
-	elm_entry_input_panel_layout_set(entry, Utils::at(inputLayout, m_Field.getSubType()));
 	evas_object_smart_callback_add(entry, "changed",
 			makeCallback(&ContactTextFieldControl::onChanged), this);
+	evas_object_smart_callback_add(entry, "focused",
+			makeCallback(&ContactTextFieldControl::onFocused), this);
 	evas_object_smart_callback_add(entry, "unfocused",
 			makeCallback(&ContactTextFieldControl::onUnfocused), this);
+	evas_object_smart_callback_add(entry, "activated",
+			makeCallback(&ContactTextFieldControl::onActivated), this);
 
+	updateEntryLayout();
 	update();
 }
 
 void ContactTextFieldControl::onChanged(Evas_Object *entry, void *eventInfo)
 {
+	if (m_IsUpdating) {
+		m_IsUpdating = false;
+	} else {
+		m_IsChanged = true;
+	}
+
 	if (m_IsEmpty != elm_entry_is_empty(entry)) {
 		m_IsEmpty = !m_IsEmpty;
 		save();
 	}
 }
 
+void ContactTextFieldControl::onFocused(Evas_Object *entry, void *eventInfo)
+{
+	m_NextItem = nullptr;
+	for (auto item = m_ParentItem->getNextItem(); item; item = item->getNextItem()) {
+		if (item->isFocusable()) {
+			m_NextItem = item;
+			break;
+		}
+	}
+
+	elm_entry_input_panel_return_key_type_set(entry, m_NextItem
+			? ELM_INPUT_PANEL_RETURN_KEY_TYPE_NEXT
+			: ELM_INPUT_PANEL_RETURN_KEY_TYPE_DONE);
+}
+
 void ContactTextFieldControl::onUnfocused(Evas_Object *entry, void *eventInfo)
 {
 	save();
+}
+
+void ContactTextFieldControl::onActivated(Evas_Object *entry, void *eventInfo)
+{
+	if (m_NextItem) {
+		m_NextItem->focus(ELM_GENLIST_ITEM_SCROLLTO_IN, true);
+	} else {
+		elm_object_focus_set(entry, EINA_FALSE);
+	}
 }
