90ecf897d4b61bf89adb4397fb8631c7a82cf11e
Eugene Kurzberg <i.kurtsberg@samsung.com>
2016-01-28 10:49:16 +0200

    TizenRefApp-5571 Implement Basic info item (Thumbnail, Name, Company, Favorite)
    Change-Id: I457ba20a485e350802023e0e750830fe92c5e9ee
    Signed-off-by: Eugene Kurzberg <i.kurtsberg@samsung.com>

---
+++ lib-contact/res/details/edje/DetailsItemLayout.h
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2015 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#ifndef DETAILS_ITEM_LAYOUT_H
+#define DETAILS_ITEM_LAYOUT_H
+
+#include "DetailsPath.h"
+
+#define DETAILS_ITEM_STYLE      "details_item"
+
+#define PART_BACK_BTN           "swallow.back"
+#define PART_FAV_BTN            "swallow.favorite"
+#define PART_THUMBNAIL          "swallow.thumbnail"
+
+#define PART_NAME               "text.name"
+#define PART_PHONETIC_NAME      "text.phonetic_name"
+#define PART_COMPANY            "text.company"
+
+#define GROUP_ICON_CALL         "icon_call"
+#define GROUP_ICON_MESSAGE      "icon_message"
+#define GROUP_ICON_EMAIL        "icon_email"
+#define GROUP_ICON_INTERNET     "icon_internet"
+
+#endif /* DETAILS_ITEM_LAYOUT_H */
