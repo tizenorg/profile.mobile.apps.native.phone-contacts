9a46d2eced921b3ff0a1ae2664ce2df74157da0b
Eugene Kurzberg <i.kurtsberg@samsung.com>
2016-01-28 11:47:19 +0200

    TizenRefApp-5584 Implement Phone, Email, Website specialized items (Call, Compose, Open Webpage)
    Change-Id: Ic231dd42b663946f82cc8a9dbcc63dbd45ab87bd
    Signed-off-by: Eugene Kurzberg <i.kurtsberg@samsung.com>

---
+++ lib-contact/inc/Contacts/Details/ActionFieldItem.h
@@ -0,0 +1,79 @@
+/*
+ * Copyright (c) 2015 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#ifndef CONTACTS_DETAILS_ACTION_FIELD_ITEM_H
+#define CONTACTS_DETAILS_ACTION_FIELD_ITEM_H
+
+#include "Contacts/Details/TypedFieldItem.h"
+#include "App/AppControl.h"
+
+namespace Contacts
+{
+	namespace Details
+	{
+		/**
+		 * @brief Genlist item representing a field which value can be used to
+		 *        initiate some external action.
+		 */
+		class ActionFieldItem : public TypedFieldItem
+		{
+		public:
+			enum ActionId
+			{
+				ActionCall,           /**< Make a telephony call using value as a number */
+				ActionComposeMessage, /**< Compose an SMS using value as recipient */
+				ActionComposeEmail,   /**< Compose an E-mail using value as an address */
+				ActionOpenWebpage     /**< Open webpage in browser using value as URL */
+			};
+
+			/**
+			 * @brief Create action item.
+			 * @param[in]   object      Contact field of TypeObject type
+			 * @param[in]   actionId    Action to perform using object's first field value
+			 */
+			ActionFieldItem(Model::ContactObject &object, ActionId actionId);
+
+		protected:
+			/**
+			 * @see GenlistItem::getContent()
+			 */
+			virtual Evas_Object *getContent(Evas_Object *parent, const char *part) override;
+
+			/**
+			 * @see GenlistItem::onSelected()
+			 */
+			virtual void onSelected() override;
+
+			/**
+			 * @brief Create action button.
+			 * @param[in]   parent      Parent widget
+			 * @param[in]   actionId    Button's action ID
+			 * @return Action button.
+			 */
+			Evas_Object *createActionButton(Evas_Object *parent, ActionId actionId);
+
+		private:
+			void executeAction(ActionId actionId);
+			void onButtonPressed(Evas_Object *button, void *eventInfo);
+
+			ActionId m_ActionId;
+			App::AppControl m_AppControl;
+		};
+	}
+}
+
+#endif /* CONTACTS_DETAILS_ACTION_FIELD_ITEM_H */
---
+++ lib-contact/src/Contacts/Details/ActionFieldItem.cpp
@@ -0,0 +1,100 @@
+/*
+ * Copyright (c) 2015 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#include "Contacts/Details/ActionFieldItem.h"
+#include "Contacts/Model/ContactTextField.h"
+
+#include "App/Path.h"
+#include "Ui/Scale.h"
+#include "Utils/Callback.h"
+
+#include "DetailsItemLayout.h"
+#include "DetailsItemLayoutMetrics.h"
+
+using namespace Contacts::Details;
+using namespace Contacts::Model;
+
+namespace
+{
+	struct {
+		const char *icon;
+		const char *operation;
+		const char *scheme;
+	} actions[] = {
+		/* ActionCall           = */ { GROUP_ICON_CALL,     APP_CONTROL_OPERATION_CALL, "tel:" },
+		/* ActionComposeMessage = */ { GROUP_ICON_MESSAGE,  APP_CONTROL_OPERATION_COMPOSE, "sms:" },
+		/* ActionComposeEmail   = */ { GROUP_ICON_EMAIL,    APP_CONTROL_OPERATION_COMPOSE, "mailto:" },
+		/* ActionOpenWebpage    = */ { GROUP_ICON_INTERNET, APP_CONTROL_OPERATION_VIEW, "" }
+	};
+
+	const std::string layoutPath = App::getResourcePath(DETAILS_ITEM_LAYOUT_EDJ);
+}
+
+ActionFieldItem::ActionFieldItem(Model::ContactObject &object, ActionId actionId)
+	: TypedFieldItem(object), m_ActionId(actionId)
+{
+}
+
+Evas_Object *ActionFieldItem::getContent(Evas_Object *parent, const char *part)
+{
+	if (strcmp(part, "elm.swallow.icon.1") == 0) {
+		return createActionButton(parent, m_ActionId);
+	} else if (m_ActionId == ActionCall && strcmp(part, "elm.swallow.icon.2") == 0) {
+		return createActionButton(parent, ActionComposeMessage);
+	}
+
+	return TypedFieldItem::getContent(parent, part);
+}
+
+void ActionFieldItem::onSelected()
+{
+	executeAction(m_ActionId);
+}
+
+Evas_Object *ActionFieldItem::createActionButton(Evas_Object *parent, ActionId actionId)
+{
+	static const int imageSize = Ui::getScaledValue(BTN_WH);
+
+	Evas_Object *image = elm_image_add(parent);
+	elm_image_file_set(image, layoutPath.c_str(), actions[actionId].icon);
+	evas_object_size_hint_min_set(image, imageSize, imageSize);
+	evas_object_propagate_events_set(image, EINA_FALSE);
+	evas_object_smart_data_set(image, (void *) actionId);
+	evas_object_smart_callback_add(image, "clicked",
+			makeCallback(&ActionFieldItem::onButtonPressed), this);
+
+	return image;
+}
+
+void ActionFieldItem::executeAction(ActionId actionId)
+{
+	auto action = actions[actionId];
+
+	std::string uri = action.scheme;
+	const char *value = getField().cast<ContactTextField>().getValue();
+	if (value) {
+		uri.append(value);
+	}
+
+	m_AppControl = App::AppControl(action.operation, nullptr, uri.c_str());
+	m_AppControl.launch();
+}
+
+void ActionFieldItem::onButtonPressed(Evas_Object *button, void *eventInfo)
+{
+	executeAction((ActionId) (long) evas_object_smart_data_get(button));
+}
