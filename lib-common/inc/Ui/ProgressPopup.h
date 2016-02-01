7527d61eafd09e470f923b418314099eff4e79e3
Nataliia Sydorchuk <n.sydorchuk@samsung.com>
2016-02-01 14:51:47 +0200

    TizenRefApp-5593 Implement Progress bar popup
    Change-Id: I0c569922ba3ea68082d2e4fd5a760d23bed0fad3
    Signed-off-by: Nataliia Sydorchuk <n.sydorchuk@samsung.com>

---
+++ lib-common/inc/Ui/ProgressPopup.h
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) 2015 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#ifndef UI_PROGRESS_POPUP_H
+#define UI_PROGRESS_POPUP_H
+
+#include "Ui/Popup.h"
+
+namespace Ui
+{
+	class EXPORT_API ProgressPopup : public Popup
+	{
+	public:
+		/**
+		 * @brief Create progress popup.
+		 * @param[in]   maxValue    Max value of progressbar
+		 */
+		ProgressPopup(size_t maxValue);
+
+		/**
+		 * @brief Update progressbar according to new value
+		 * @param[in]   value   Progress value
+		 */
+		void updateProgressbar(size_t value);
+
+	private:
+		virtual Evas_Object *onCreate(Evas_Object *parent) override;
+
+	private:
+		size_t m_MaxValue;
+		Evas_Object *m_Progressbar;
+	};
+}
+
+#endif /* UI_PROGRESS_POPUP_H */
