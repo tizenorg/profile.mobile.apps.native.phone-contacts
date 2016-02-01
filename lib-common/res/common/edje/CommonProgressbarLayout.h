b73eed811b2788a5518b6655af9d1e2796288b19
Nataliia Sydorchuk <n.sydorchuk@samsung.com>
2016-02-01 16:23:07 +0200

    TizenRefApp-5593 Implement Progress bar popup
    Change-Id: I0c569922ba3ea68082d2e4fd5a760d23bed0fad3
    Signed-off-by: Nataliia Sydorchuk <n.sydorchuk@samsung.com>

---
+++ lib-common/res/common/edje/CommonProgressbarLayout.h
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2015 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#ifndef COMMON_PROGRESSBAR_LAYOUT_H
+#define COMMON_PROGRESSBAR_LAYOUT_H
+
+#include "CommonPath.h"
+
+#define COMMON_PROGRESSBAR_LAYOUT_EDJ      COMMON_EDJ_DIR"common-progressbar-layout.edj"
+
+#define GROUP_PROGRESSBAR_LAYOUT       "progressbar_layout"
+
+#define PART_PROGRESSBAR           "swallow.progressbar"
+
+#define PROGRESSBAR_PAD_X          32
+#define PROGRESSBAR_PAD_Y          17
+
+#endif /* COMMON_PROGRESSBAR_LAYOUT_H */
