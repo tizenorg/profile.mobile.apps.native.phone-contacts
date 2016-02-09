825d3e7ec67e7bd7ba89a750a9c14345249efe67
Iryna Ferenchak <i.ferenchak@samsung.com>
2016-02-09 14:00:21 +0200

    TizenRefApp-5388 Implement Log Item
    Change-Id: I5f34ae9664c72c14e31d03440664a378004db004
    Signed-off-by: Iryna Ferenchak <i.ferenchak@samsung.com>

---
+++ lib-logs/res/logs/edje/LogItemLayout.h
@@ -0,0 +1,14 @@
+#ifndef LOG_ITEM_LAYOUT_H
+#define LOG_ITEM_LAYOUT_H
+
+#define ICON_INCOMING             "icon_incoming"
+#define ICON_OUTGOING             "icon_outgoing"
+#define ICON_MISSED               "icon_missed"
+#define ICON_REJECTED             "icon_rejected"
+#define ICON_AUTO_REJECTED        "icon_auto_rejected"
+#define ICON_INFO                 "icon_info"
+
+#define THUMBNAIL_PART            "swallow.content"
+#define LOG_TYPE_PART             "swallow.call.type"
+
+#endif /* LOG_ITEM_LAYOUT_H */
---
+++ lib-logs/res/logs/edje/LogPath.h
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2015 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#ifndef LOG_PATH_H
+#define LOG_PATH_H
+
+#define LOG_ITEM                  "log_item"
+#define LOG_IMG_DIR               "logs/images/"
+#define LOG_EDJ_DIR               "logs/edje/"
+#define LOG_ITEM_LAYOUT_EDJ        LOG_EDJ_DIR"log_item.edj"
+
+#define LOG_ICON_INFO             LOG_IMG_DIR"logs_icon_info.png"
+#define LOG_ICON_CONTAINER        LOG_IMG_DIR"logs_icon_badge_container_02.png"
+#define LOG_ICON_INCOMING         LOG_IMG_DIR"logs_icon_in.png"
+#define LOG_ICON_OUTGOING         LOG_IMG_DIR"logs_icon_out.png"
+#define LOG_ICON_MISSED           LOG_IMG_DIR"logs_icon_missed.png"
+#define LOG_ICON_REJECTED         LOG_IMG_DIR"logs_icon_reject.png"
+#define LOG_ICON_AUTO_REJECTED    LOG_IMG_DIR"logs_icon_reject_auto.png"
+
+#endif /* LOG_PATH_H */
