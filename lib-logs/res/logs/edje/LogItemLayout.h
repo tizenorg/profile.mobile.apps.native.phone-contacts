610a02b866d01963f29a7adee5abd9c6bf948b52
Iryna Ferenchak <i.ferenchak@samsung.com>
2016-02-09 15:21:29 +0200

    TizenRefApp-5388 Implement Log Item
    Change-Id: I5f34ae9664c72c14e31d03440664a378004db004
    Signed-off-by: Iryna Ferenchak <i.ferenchak@samsung.com>

---
+++ lib-logs/res/logs/edje/LogItemLayout.h
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2015 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#ifndef LOG_ITEM_LAYOUT_H
+#define LOG_ITEM_LAYOUT_H
+
+#define ICON_INCOMING             "icon_incoming"
+#define ICON_OUTGOING             "icon_outgoing"
+#define ICON_MISSED               "icon_missed"
+#define ICON_REJECTED             "icon_rejected"
+#define ICON_AUTO_REJECTED        "icon_auto_rejected"
+#define ICON_INFO                 "icon_info"
+
+#define THUMBNAIL_PART            "swallow.content"
+#define LOG_TYPE_PART             "swallow.call.type"
+
+#endif /* LOG_ITEM_LAYOUT_H */
