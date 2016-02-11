08e8d40cd48cf664fe3cd41a08af1e56f0e396f0
Iryna Ferenchak <i.ferenchak@samsung.com>
2016-02-11 11:58:54 +0200

    Create Group log item
    Change-Id: I6e100d5d7b3f96b9162391add131b0a20c9458e8
    Signed-off-by: Iryna Ferenchak <i.ferenchak@samsung.com>

---
+++ lib-logs/src/Logs/List/LogGroupItem.cpp
@@ -0,0 +1,66 @@
+/*
+ * Copyright (c) 2015 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#include "Logs/List/LogGroupItem.h"
+#include <app_i18n.h>
+
+using namespace Logs::List;
+
+#define PART_LOG_DATE           "elm.text"
+#define BUFFER_SIZE             32
+
+LogGroupItem::LogGroupItem(const tm date)
+	: m_Date(date)
+{ }
+
+const tm LogGroupItem::getDate() const
+{
+	return m_Date;
+}
+
+char *LogGroupItem::getText(Evas_Object *parent, const char *part)
+{
+	if (strcmp(part, PART_LOG_DATE) == 0) {
+		return convertDate(m_Date);
+	}
+
+	return nullptr;
+}
+
+char *LogGroupItem::convertDate(const tm &date)
+{
+	time_t nowTime = time(NULL);
+	struct tm nowDate;
+	localtime_r(&nowTime, &nowDate);
+
+	if(itemDate.tm_year == nowDate.tm_year &&
+			itemDate.tm_mon == nowDate.tm_mon &&
+			itemDate.tm_mday == nowDate.tm_mday) {
+		return strdup(_("IDS_LOGS_HEADER_TODAY"));
+	} else if(itemDate.tm_year == nowDate.tm_year &&
+				itemDate.tm_mon == nowDate.tm_mon &&
+				((itemDate.tm_mday)+1) == nowDate.tm_mday ) {
+		return strdup(_("IDS_LOGS_HEADER_YESTERDAY"));
+	} else {
+		tm date = nowDate;
+		char buffer[BUFFER_SIZE];
+		strftime(buffer, sizeof(buffer), "%x", &date);
+		return strdup(buffer);
+	}
+
+	return nullptr;
+}
