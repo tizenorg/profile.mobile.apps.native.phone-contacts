a5acf7d0521dc298a5377f82f6262ec0bdefd5ec
Nataliia Kamyshna <n.kamyshna@samsung.com>
2016-02-29 12:47:17 +0200

    TizenRefApp-5841 Implement use of LOG_DETAILS_ITEM_LAYOUT_EDJ theme for Logs/BasicInfoItem
    Change-Id: Ia9acd98e9a128a1d3db5390df6f65bd5b5a699ca
    Signed-off-by: Nataliia Kamyshna <n.kamyshna@samsung.com>

---
+++ lib-logs/res/details/edje/LogsDetailsItemLayoutMetrics.h
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2015 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#ifndef LOGS_DETAILS_ITEM_LAYOUT_METRICS_H
+#define LOGS_DETAILS_ITEM_LAYOUT_METRICS_H
+
+#include "../../../../lib-common/res/details/edje/DetailsItemLayoutMetrics.h"
+
+#define LOGS_THUMBNAIL_PAD_T 46
+
+#define NAME_PAD_T 22
+#define NAME_PAD_B 59
+
+#define UNSAVED_BTN_PAD_T 46
+#define UNSAVED_BTN_PAD_LR 32
+#define UNSAVED_BTN_PAD_BETWEEN 16
+#define UNSAVED_BTN_PAD_B 30
+
+#define UNSAVED_BTN_H 78
+
+#endif /* LOGS_DETAILS_ITEM_LAYOUT_METRICS_H */
---
+++ lib-logs/res/details/edje/LogsDetailsPath.h
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2015 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#ifndef LOGS_DETAILS_PATH_H
+#define LOGS_DETAILS_PATH_H
+
+#define DETAILS_IMG_DIR "details/images/"
+#define DETAILS_EDJ_DIR "details/edje/"
+
+#define DETAILS_ITEM_LAYOUT_EDJ   DETAILS_EDJ_DIR"logs-details-item-layout.edj"
+
+#define DETAILS_ICON_CALL         DETAILS_IMG_DIR"logs_btn_ic_call.png"
+#define DETAILS_ICON_MESSAGE      DETAILS_IMG_DIR"logs_btn_ic_message.png"
+
+#endif /* LOGS_DETAILS_PATH_H */
