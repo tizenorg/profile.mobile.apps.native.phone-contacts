9939bfdcc0bab51188fe03519587dfbbb6a054d8
Nataliia Kamyshna <n.kamyshna@samsung.com>
2016-03-01 18:50:20 +0200

    TizenRefApp-5844 Implement Logs/BasicInfoItem
    Change-Id: Idbe10b6e5f945233c98fe873174fc64e6644c889
    Signed-off-by: Nataliia Kamyshna <n.kamyshna@samsung.com>

---
+++ lib-logs/src/Logs/Details/BasicInfoItem.cpp
@@ -0,0 +1,153 @@
+/*
+ * Copyright (c) 2015 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#include "Logs/Details/BasicInfoItem.h"
+
+#include "Ui/Genlist.h"
+#include "Ui/Thumbnail.h"
+#include "Utils/Callback.h"
+#include "App/AppControlRequest.h"
+#include "AppsCommonButtons.h"
+
+#include "Logs/Model/LogGroup.h"
+#include "Logs/Model/Log.h"
+
+#include "LogsDetailsItemLayout.h"
+#include "LogsDetailsItemLayoutMetrics.h"
+
+using namespace Logs::Details;
+using namespace Logs::Model;
+
+BasicInfoItem::BasicInfoItem(LogGroup *group)
+	: m_Group(group)
+{
+	m_Log = m_Group->getLogList().back();
+}
+
+void BasicInfoItem::setBackCallback(BackCallback callback)
+{
+	m_OnBackPressed = std::move(callback);
+}
+
+Elm_Genlist_Item_Class *BasicInfoItem::getItemClass() const
+{
+	static Elm_Genlist_Item_Class itc = createItemClass(LOGS_DETAILS_ITEM_STYLE);
+	return &itc;
+}
+
+char *BasicInfoItem::getText(Evas_Object *parent, const char *part)
+{
+	const char *name = m_Log->getName();
+	if (isSavedLog() && strcmp(part, PART_NAME) == 0) {
+		return strdup(name);
+	}
+
+	return nullptr;
+}
+
+Evas_Object *BasicInfoItem::getContent(Evas_Object *parent, const char *part)
+{
+	if (strcmp(part, PART_BACK_BTN) == 0) {
+		return createBackButton(parent);
+	} else if (strcmp(part, PART_THUMBNAIL) == 0) {
+		auto control = Ui::Thumbnail::create(parent, Ui::Thumbnail::SizeLarge, m_Log->getImagePath());
+		return control->getEvasObject();
+	} else if (!isSavedLog() && strcmp(part, PART_UNSAVED_BTNS) == 0) {
+		return createUnsavedButtons(parent);
+	}
+	return nullptr;
+}
+
+Eina_Bool BasicInfoItem::getState(Evas_Object *parent, const char *part)
+{
+	return isSavedLog();
+}
+
+void BasicInfoItem::onSelected()
+{
+	if (isSavedLog()) {
+		m_AppControl = App::requestContactDetails(m_Log->getPersonId());
+		m_AppControl.launch();
+	}
+}
+
+bool BasicInfoItem::isSavedLog()
+{
+	return m_Log->getPersonId() > 0;
+}
+
+Evas_Object *BasicInfoItem::createBackButton(Evas_Object *parent)
+{
+	Evas_Object *button = elm_button_add(parent);
+	elm_object_style_set(button, "naviframe/back_btn/default");
+	evas_object_smart_callback_add(button, "clicked",
+			makeCallback(&BasicInfoItem::onBackPressed), this);
+	return button;
+}
+
+Evas_Object *BasicInfoItem::createUnsavedButtons(Evas_Object *parent)
+{
+	Evas_Object *box = elm_box_add(parent);
+	elm_box_padding_set(box, UNSAVED_BTN_PAD_BETWEEN, 0);
+	elm_box_horizontal_set(box, EINA_TRUE);
+
+	createUnsavedButton(box, "IDS_LOGS_BUTTON_CREATE_CONTACT_ABB",
+			(Evas_Smart_Cb) makeCallback(&BasicInfoItem::onCreatePressed));
+
+	createUnsavedButton(box, "IDS_LOGS_BUTTON_UPDATE_CONTACT_ABB2",
+			(Evas_Smart_Cb) makeCallback(&BasicInfoItem::onUpdatePressed));
+
+	return box;
+}
+
+Evas_Object *BasicInfoItem::createUnsavedButton(Evas_Object *box, const char *text, Evas_Smart_Cb func)
+{
+	Evas_Object *button = elm_button_add(box);
+	elm_object_style_set(button, "custom_default");
+
+	Evas_Object *edje = elm_layout_edje_get(button);
+	edje_object_color_class_set(edje, BUTTON_COLOR_CLASS_NORMAL, COLOR_UNSAVED_BUTTON_NORMAL,
+			0, 0, 0, 0, 0, 0, 0, 0);
+	edje_object_color_class_set(edje, BUTTON_COLOR_CLASS_PRESSED, COLOR_UNSAVED_BUTTON_PRESSED,
+			0, 0, 0, 0, 0, 0, 0, 0);
+
+	elm_object_translatable_text_set(button, text);
+	evas_object_smart_callback_add(button, "clicked", func, this);
+
+	evas_object_show(button);
+	elm_box_pack_end(box, button);
+	return button;
+}
+
+void BasicInfoItem::onBackPressed(Evas_Object *button, void *eventInfo)
+{
+	if (m_OnBackPressed) {
+		m_OnBackPressed();
+	}
+}
+
+void BasicInfoItem::onCreatePressed()
+{
+	m_AppControl = App::requestContactCreate(m_Log->getNumber());
+	m_AppControl.launch();
+}
+
+void BasicInfoItem::onUpdatePressed()
+{
+	m_AppControl = App::requestContactEdit(0, m_Log->getNumber());
+	m_AppControl.launch();
+}
