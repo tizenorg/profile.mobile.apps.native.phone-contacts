effbc52ef0b6b68157d1b0f52ae4189ced6216e8
Aleksandr Sapozhnik <a.sapozhnik@samsung.com>
2016-03-21 14:04:03 +0200

    Implemented manage favorites controller
    Change-Id: Ide47995f44715c2247924f8a10357e711a809616
    Signed-off-by: Aleksandr Sapozhnik <a.sapozhnik@samsung.com>

---
+++ lib-contact/src/Contacts/List/ManageFavoritesPopup.cpp
@@ -0,0 +1,61 @@
+/*
+ * Copyright (c) 2016 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#include "Contacts/List/ListView.h"
+#include "Contacts/List/ManageFavoritesPopup.h"
+
+#include "Ui/Navigator.h"
+
+using namespace Contacts::List;
+using namespace Ui;
+
+ManageFavoritesPopup::ManageFavoritesPopup(Navigator *navigator)
+	: m_Navigator(navigator)
+{
+}
+
+void ManageFavoritesPopup::onCreated()
+{
+	ListPopup::onCreated();
+
+	setTitle("IDS_PB_HEADER_MANAGE_FAVOURITES_ABB");
+
+	addItem("IDS_PB_OPT_ADD", std::bind(&ManageFavoritesPopup::onAdd, this));
+	addItem("IDS_PB_OPT_REORDER", std::bind(&ManageFavoritesPopup::onReorder, this));
+	addItem("IDS_PB_OPT_REMOVE", std::bind(&ManageFavoritesPopup::onRemove, this));
+}
+
+void ManageFavoritesPopup::onAdd()
+{
+	ListView *view = new ListView();
+	view->setSelectMode(SelectMulti);
+	view->setSelectCallback([](SelectResults results) {
+		//todo
+		return true;
+	});
+	m_Navigator->navigateTo(view);
+}
+
+void ManageFavoritesPopup::onReorder()
+{
+	//todo
+}
+
+void ManageFavoritesPopup::onRemove()
+{
+	//todo
+}
