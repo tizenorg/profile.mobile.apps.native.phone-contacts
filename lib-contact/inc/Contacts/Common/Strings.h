f356e1cf3c5a3d860f7da6596b9354de71c6ea6b
Eugene Kurzberg <i.kurtsberg@samsung.com>
2016-03-24 10:02:15 +0200

    Moved Contacts enum types and strings to lib-common.
    Change-Id: I64f703df20eacb723c48c1ca0238cefd575f42af
    Signed-off-by: Eugene Kurzberg <i.kurtsberg@samsung.com>

--- lib-contact/inc/Contacts/Common/Strings.h
+++ lib-contact/inc/Contacts/Common/Strings.h
@@ -19,8 +19,6 @@
 #define CONTACTS_COMMON_STRINGS_H
 
 #include "Contacts/Model/ContactFields.h"
-#include "Contacts/Model/ContactTypes.h"
-#include "Utils/Range.h"
 
 namespace Contacts
 {
@@ -44,31 +42,6 @@ namespace Contacts
 		 * @see ContactPhoneticNameFieldId
 		 */
 		const char *getContactChildFieldName(unsigned fieldId);
-
-		/**
-		 * @brief Contact enum value-name pair.
-		 * @see ContactEnumType
-		 */
-		struct ContactEnumValueName
-		{
-			int value;          /**< Enum value */
-			const char *name;   /**< Translatable value name */
-		};
-
-		/**
-		 * @brief Get contact enum value names.
-		 * @param[in]   type    Enum type
-		 * @return Enum value translatable names range.
-		 */
-		Utils::Range<const ContactEnumValueName *> getContactEnumValueNames(Model::ContactEnumType type);
-
-		/**
-		 * @brief Get translatable name for enum value.
-		 * @param[in]   type    Enum type
-		 * @param[in]   value   Enum value
-		 * @return Enum value translatable name.
-		 */
-		const char *getContactEnumValueName(Model::ContactEnumType type, int value);
 	}
 }
 
