a6fdc6a5f29a3b23277842de2cedc303d6b70ac5
Nataliia Sydorchuk <n.sydorchuk@samsung.com>
2016-04-25 12:50:24 +0300

    TizenRefApp-6086 Implement provider of most frequent contacts
    Change-Id: Id350e53f28e7f8515b5bb891dea7f94c99143714
    Signed-off-by: Nataliia Sydorchuk <n.sydorchuk@samsung.com>

---
+++ lib-contacts/inc/Contacts/List/Model/MfcProvider.h
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) 2015 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#ifndef CONTACTS_LIST_MODEL_MFC_PROVIDER_H
+#define CONTACTS_LIST_MODEL_MFC_PROVIDER_H
+
+#include "Contacts/List/Model/PersonProvider.h"
+
+namespace Contacts
+{
+	namespace Model
+	{
+		class ContactData;
+	}
+	namespace List
+	{
+		namespace Model
+		{
+			/**
+			 * @brief Provides list of most frequent contacts(mfc).
+			 */
+			class MfcProvider : public PersonProvider
+			{
+			public:
+				MfcProvider();
+				virtual ~MfcProvider() override;
+
+			private:
+				virtual contacts_list_h getPersonList() const override;
+				contacts_list_h getPersonUsageList() const;
+
+				void onPersonUsageUpdate(const char *viewUri);
+
+				void updateMfcList(contacts_list_h list);
+			};
+		}
+	}
+}
+
+#endif /* CONTACTS_LIST_MODEL_MFC_PROVIDER_H */
