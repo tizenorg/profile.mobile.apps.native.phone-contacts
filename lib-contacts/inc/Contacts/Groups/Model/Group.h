1b5b7f6e279ec1ccd2971ffd4a4f012a5e7fb34c
Nataliia Sydorchuk <n.sydorchuk@samsung.com>
2016-06-08 10:43:28 +0300

    TizenRefApp-6451 Implement GroupProvider [Implementation] Implemented Group and GroupProvider
    Change-Id: I91569e97f5a41448a12dd645b1b759877005124e
    Signed-off-by: Nataliia Sydorchuk <n.sydorchuk@samsung.com>

---
+++ lib-contacts/inc/Contacts/Groups/Model/Group.h
@@ -0,0 +1,81 @@
+/*
+ * Copyright (c) 2015-2016 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#ifndef CONTACTS_GROUPS_MODEL_GROUP_H
+#define CONTACTS_GROUPS_MODEL_GROUP_H
+
+#include "Model/DataItem.h"
+#include "Utils/UniString.h"
+
+#include <contacts.h>
+
+namespace Contacts
+{
+	namespace Groups
+	{
+		namespace Model
+		{
+			class Group : public ::Model::DataItem
+			{
+			public:
+				/**
+				 * @brief Create group object
+				 * @param[in]   record  _contacts_group record
+				 */
+				Group(contacts_record_h record);
+				virtual ~Group() override;
+
+				/**
+				 * @return Person ID
+				 */
+				int getId() const;
+
+				/**
+				 * @return Name
+				 */
+				const char *getName() const;
+
+				/**
+				 * @return Ringtone path
+				 */
+				const char *getRingtone() const;
+
+				/**
+				 * @return Group record
+				 */
+				contacts_record_h getRecord() const;
+
+				/**
+				 * @brief Compares group's values
+				 * @return True if sort value less than in @a that, otherwise false
+				 */
+				bool operator<(const Group &that) const;
+
+			private:
+				friend class GroupsProvider;
+
+				const Utils::UniString &getSortValue() const;
+				void update(contacts_record_h record);
+
+				contacts_record_h m_Record;
+				mutable Utils::UniString m_SortValue;
+			};
+		}
+	}
+}
+
+#endif /* CONTACTS_GROUPS_MODEL_GROUP_H */
---
+++ lib-contacts/inc/Contacts/Groups/Model/GroupsProvider.h
@@ -0,0 +1,64 @@
+/*
+ * Copyright (c) 2015-2016 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#ifndef CONTACTS_GROUPS_MODEL_GROUPS_PROVIDER_H
+#define CONTACTS_GROUPS_MODEL_GROUPS_PROVIDER_H
+
+#include "Model/DataProvider.h"
+
+#include <contacts.h>
+
+namespace Contacts
+{
+	namespace Groups
+	{
+		namespace Model
+		{
+			class Group;
+
+			class GroupsProvider : public ::Model::DataProvider
+			{
+			public:
+				GroupsProvider();
+				virtual ~GroupsProvider() override;
+
+				/**
+				 * @brief Get group list
+				 * @return List of group objects
+				 */
+				virtual const DataList &getDataList() override;
+				virtual void clearDataList() override;
+
+			private:
+				void onChanged(const char *uri);
+				void insertGroup(int id);
+				void updateGroup(DataList::const_iterator groupIt);
+				void deleteGroup(DataList::const_iterator groupIt);
+
+				Group *createGroup(contacts_record_h record);
+				DataList::const_iterator findGroup(int id);
+
+				int m_DbVersion;
+				DataList m_List;
+			};
+		}
+	}
+}
+
+
+
+#endif /* CONTACTS_GROUPS_MODEL_GROUPS_PROVIDER_H */
