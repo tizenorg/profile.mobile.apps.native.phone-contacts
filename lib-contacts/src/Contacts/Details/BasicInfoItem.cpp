b3258746c14f0b7e4529e9517d589e78df68f7e5
Aleksandr Sapozhnik <a.sapozhnik@samsung.com>
2016-06-10 15:10:40 +0300

    Fixed new Svace issues
    Change-Id: Ic21003e0441c3c24bd7e18a0c0d285e58158d2bb
    Signed-off-by: Aleksandr Sapozhnik <a.sapozhnik@samsung.com>

--- lib-contacts/src/Contacts/Details/BasicInfoItem.cpp
+++ lib-contacts/src/Contacts/Details/BasicInfoItem.cpp
@@ -24,6 +24,7 @@
 #include "Ui/Genlist.h"
 #include "Ui/Thumbnail.h"
 #include "Utils/Callback.h"
+#include "Utils/String.h"
 
 #include "ContactsDetailsItemLayout.h"
 
@@ -71,7 +72,7 @@ Elm_Genlist_Item_Class *BasicInfoItem::getItemClass() const
 char *BasicInfoItem::getText(Evas_Object *parent, const char *part)
 {
 	if (strcmp(part, PART_NAME) == 0) {
-		return strdup(m_Name.getValue());
+		return Utils::safeDup(m_Name.getValue());
 	} else if (strcmp(part, PART_PHONETIC_NAME) == 0) {
 		return strdup(m_PhoneticName.getValue().c_str());
 	} else if (strcmp(part, PART_COMPANY) == 0) {
