e24104c3f8dc753c326cea1c8b193fb64bfa0e7d
Nataliia Sydorchuk <n.sydorchuk@samsung.com>
2016-07-01 16:02:00 +0300

    TizenRefApp-6592 Implement Create group view
    Change-Id: I9f4632e476c5a13f4c499f65b3462611010f6772
    Signed-off-by: Nataliia Sydorchuk <n.sydorchuk@samsung.com>

---
+++ lib-contacts/res/groups/edje/GroupItemLayout.h
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2015-2016 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#ifndef GROUP_ITEM_LAYOUT_H
+#define GROUP_ITEM_LAYOUT_H
+
+#include "GroupPath.h"
+
+#define GROUP_NAME_ITEM_STYLE          "group_name"
+#define GROUP_ADD_MEMBERS_ITEM_STYLE   "group_add_members"
+
+#define PART_GROUP_NAME_LABEL  "text.label"
+#define PART_GROUP_NAME_VALUE  "swallow.value"
+
+#define PART_GROUP_ADD_MEMBERS_LABEL   "text.label"
+#define PART_GROUP_ADD_MEMBERS_COUNTER "text.counter"
+#define PART_GROUP_ADD_MEMBERS_BUTTON  "swallow.button"
+
+#endif /* GROUP_ITEM_LAYOUT_H */
