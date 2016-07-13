e92efdf4c8546dcafdb3103fd5babe26ac28a4ce
Eugene Kurzberg <i.kurtsberg@samsung.com>
2016-07-13 13:16:24 +0300

    Implemented common popup with editfield.
    Change-Id: I1e287987b73d2ad3273b04299088eee01251849c
    Signed-off-by: Eugene Kurzberg <i.kurtsberg@samsung.com>

--- lib-apps-common/src/Ux/EditfieldPopup.cpp
+++ lib-apps-common/src/Ux/EditfieldPopup.cpp
@@ -15,61 +15,73 @@
  *
  */
 
-#include "Contacts/Input/ContactTypedFieldLabelPopup.h"
+#include "Ux/EditfieldPopup.h"
 #include "Ui/Editfield.h"
 #include "Utils/Callback.h"
 
-using namespace Contacts::Input;
+using namespace Ui;
+using namespace Ux;
 
-ContactTypedFieldLabelPopup::ContactTypedFieldLabelPopup()
-	: m_Editfield(nullptr)
+EditfieldPopup::EditfieldPopup()
+	: m_Editfield(nullptr), m_Strings{ nullptr }
 {
 }
 
-void ContactTypedFieldLabelPopup::setResultCallback(ResultCallback callback)
+void EditfieldPopup::setStrings(Strings strings)
+{
+	m_Strings = strings;
+}
+
+void EditfieldPopup::setResultCallback(ResultCallback callback)
 {
 	m_OnResult = std::move(callback);
 }
 
-void ContactTypedFieldLabelPopup::onCreated()
+Editfield *EditfieldPopup::getEditfield() const
 {
-	setTitle("IDS_PB_HEADER_ENTER_CUSTOM_LABEL_ABB");
+	return m_Editfield;
+}
 
-	m_Editfield = Ui::Editfield::create(getEvasObject(), "IDS_PB_NPBODY_CUSTOM_TYPE_ABB");
+void EditfieldPopup::onCreated()
+{
+	m_Editfield = Editfield::create(getEvasObject(), m_Strings.guideText);
+
+	setTitle(m_Strings.popupTitle);
 	setContent(m_Editfield->getEvasObject());
-	addButton("IDS_PB_BUTTON_CANCEL");
-	Evas_Object *button = addButton("IDS_PB_BUTTON_CREATE_ABB2", [this] {
-		onCreatePressed();
-		return true;
-	});
-	elm_object_disabled_set(button, EINA_TRUE);
+	addButton(m_Strings.buttonCancel);
 
+	Evas_Object *button = addButton(m_Strings.buttonDone, std::bind(&EditfieldPopup::onDoneButtonPressed, this));
 	Evas_Object *entry = m_Editfield->getEntry();
 	elm_entry_input_panel_return_key_autoenabled_set(entry, EINA_TRUE);
 	elm_entry_input_panel_return_key_type_set(entry, ELM_INPUT_PANEL_RETURN_KEY_TYPE_DONE);
+
 	evas_object_smart_callback_add(entry, "changed",
-			(Evas_Smart_Cb) &ContactTypedFieldLabelPopup::onEntryChanged, button);
+			(Evas_Smart_Cb) &EditfieldPopup::onEntryChanged, button);
 	evas_object_smart_callback_add(entry, "activated",
-			makeCallback(&ContactTypedFieldLabelPopup::onDonePressed), this);
+			makeCallback(&EditfieldPopup::onDoneKeyPressed), this);
+
+	elm_object_disabled_set(button, EINA_TRUE);
 	elm_object_focus_set(entry, EINA_TRUE);
 }
 
-void ContactTypedFieldLabelPopup::onCreatePressed()
+bool EditfieldPopup::onDoneButtonPressed()
 {
 	if (m_OnResult) {
 		char *text = elm_entry_markup_to_utf8(elm_entry_entry_get(m_Editfield->getEntry()));
 		m_OnResult(text);
 		free(text);
 	}
+
+	return true;
 }
 
-void ContactTypedFieldLabelPopup::onDonePressed(Evas_Object *entry, void *eventInfo)
+void EditfieldPopup::onDoneKeyPressed(Evas_Object *entry, void *eventInfo)
 {
-	onCreatePressed();
+	onDoneButtonPressed();
 	delete this;
 }
 
-void ContactTypedFieldLabelPopup::onEntryChanged(Evas_Object *button, Evas_Object *entry, void *eventInfo)
+void EditfieldPopup::onEntryChanged(Evas_Object *button, Evas_Object *entry, void *eventInfo)
 {
 	elm_object_disabled_set(button, elm_entry_is_empty(entry));
 }
